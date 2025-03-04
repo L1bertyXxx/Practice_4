69f1f028f790c214c97db8dcee1a011f0904ce3a
L1berty <larsanik92@gmail.com>
2025-03-04 19:47:23 +0300

    Task4_3

--- Task_2.py
+++ Task_2.py
@@ -1,14 +1,14 @@
 place = int(input("Введите номер вашего места"))
-if place > 96 or place < 0:
+if place > 52 or place < 1:
     print("Неверное значение")
 else:
     if place % 2 == 0:
-            if int(place) > 54:
+            if int(place) >= 36:
                 print("Ваше место верхнее, боковое")
             else:
                 print("Ваше место нижнее, боковое")
     else:
-        if int(place) > 54:
+        if int(place) > 36:
             print("Ваше место верхнее, купе")
         else:
             print("Ваше место ниижнее, купе")
